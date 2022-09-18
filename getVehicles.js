b595cab89972419215111fc26e3787c93dfdc87f
ingeliza <ingelaelizabeth.rohusaar@tptlive.ee>
2022-09-18 20:29:37 +0300

    filter data by speed

--- getVehicles.js
+++ getVehicles.js
@@ -3,6 +3,12 @@ import fetch from "node-fetch";
 
 
 
+function filterSpeed(value){
+    if (value.speed =! 0 && value.speed >= 10 && value.speed <=100){
+
+    }
+}
+
 async function getVehicles(){
     let response = await fetch('', 
                                 {method: 'GET',
@@ -14,7 +20,8 @@ async function getVehicles(){
     let data = await response.json();
     console.log(data.length);
 
-    
+    let filterData = data.filter(filterSpeed);
+    console.log(filterData);
 
     //document.getElementById('data').innerHTML = data;
 }
