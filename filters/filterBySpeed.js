ab4cd94b00d37f4fd32f504a4a6dc9cea5e638e6
Ingela <59916738+ingeliza@users.noreply.github.com>
2022-10-03 12:50:32 +0300

    Clean code

--- filters/filterBySpeed.js
+++ filters/filterBySpeed.js
@@ -1,7 +1,7 @@
 export default function filterSpeed(value){
     let speed = value.speed;
     
-    if (speed =! 0 && speed >= 10 && speed <=100){
+    if (speed =! 0 && speed >= 10 && speed <= 100){
         return true;
     }
     else{
